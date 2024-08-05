27c1c8a409a85dc8fef82fc9d87e8775621e4b51
kadeksuryaindrawan <kadeksuryaindrawan@gmail.com>
2024-08-05 21:44:24 +0800

    :bug: fixed-bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,12 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-	<meta name="keywords" content="" />
-	<meta name="author" content="" />
-	<meta name="robots" content="" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Boltz : Crypto Admin Template" />
-	<meta property="og:title" content="Boltz : Crypto Admin Template" />
-	<meta property="og:description" content="Boltz : Crypto Admin Template" />
-	<meta property="og:image" content="https://boltz.dexignzone.com/xhtml/social-image.png" />
-	<meta name="format-detection" content="telephone=no">
+	<meta name="keywords" content="SRBP : Sistem Rekomendasi Program Bantuan" />
+    <meta name="author" content="SRB" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="SRBP : Sistem Rekomendasi Program Bantuan" />
+    <meta property="og:title" content="SRBP : Sistem Rekomendasi Program Bantuan" />
+    <meta property="og:description" content="SRBP : Sistem Rekomendasi Program Bantuan" />
 
 	<!-- PAGE TITLE HERE -->
 	<title>SRPB -
